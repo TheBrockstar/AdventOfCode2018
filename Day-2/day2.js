745f2376d3fd224ce3c260e5a027de503a0427e0
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-12-01 23:36:18 -0700

    Finish Day 2 Problems - Besides the solution to day 2, I also commented out all solutions to allow   for console room for tomorrow's problem.

---
+++ Day-2/day2.js
@@ -0,0 +1,348 @@
+const test = `fonbsmjyqugrapsczckghtvdxl
+fonpsmjyquwrnpeczikghtvdxw
+fonbsmdymuwrapexzikghtvdxl
+fonwsmjyquwrapeczikghttdpl
+fonbsmjkquwrapeczjkghtvdxx
+yonbsmjyquwrapecgikghtvdxc
+donbsmjyquqrapeczikghtadxl
+monbsmjyquprgpeczikghtvdxl
+fonbsmjyquwvapecqgkghtvdxl
+fonbsmjyquwrkphczikghsvdxl
+fonbomjyeuwvapeczikghtvdxl
+fonwsmjyjuwrapoczikghtvdxl
+foybsmjyquwcapeczikghsvdxl
+fonbsmjyquwrtaeczikgptvdxl
+ponbsmpyquwjapeczikghtvdxl
+flnbcmjyquwrqpeczikghtvdxl
+fonbsmjyquwrapegzikvbtvdxl
+fonbjmjyqgwrazeczikghtvdxl
+zoabsmjyquwkapeczikghtvdxl
+fonbsmjyquwrapecziktxkvdxl
+fonbsxjyrpwrapeczikghtvdxl
+fonbsmjbquwqapeciikghtvdxl
+lonbsmjyquwraphczikghtvdul
+ftnbsmjyquwrapcczikghtxdxl
+fonbsmjyqgwrapeczikghtldxc
+fonbsmjsquwmapeyzikghtvdxl
+fonbsmjyqfwrapecziqghtgdxl
+yonbsmjyquwraveczikgftvdxl
+fovbsmjyquwrapeczikggkvdxl
+fonbsmjyquwrapezzikghbvdvl
+fonzsmxyquwrapeczukghtvdxl
+fonbemjyquwrapevzikghtvrxl
+conbsxjxquwrapeczikghtvdxl
+fonbsmjsmewrapeczikghtvdxl
+folbsmjyqhwrapqczikghtvdxl
+fonbsmjyquwrzneczikghtvdxn
+fonbsmjyquirapeczikjhtvdll
+fontsmgyquwrgpeczikghtvdxl
+fonbsmjyauwrapeczbfghtvdxl
+ftnbsmjyquwrapecpifghtvdxl
+fonvsmjyqewrapeczikghlvdxl
+fonbsljyquwrapecziklhtvdxw
+fonbbmjyquwrapeczikghvadxl
+ponbsmjyquwrspeczikghivdxl
+fonbsmjcquwrapeccikghtvuxl
+fonbsmjnquwrapetzikghtvlxl
+fonbsmjymuwrapeczieghtvdxr
+ffnbsxnyquwrapeczikghtvdxl
+fonbsmjytuwrajeczzkghtvdxl
+fonssmjyquwhapeczikghkvdxl
+fonbsajyuuwrapeczikghlvdxl
+fonbsmjyquwrapeczihghtcixl
+fohbsmjyquwrapzczirghtvdxl
+fonbsmjyquwrapecjqnghtvdxl
+fonbsmjytuhrapeczihghtvdxl
+foabumjyquwrapeczikghtvdxz
+conbsmjyqtwrapeczikggtvdxl
+fonbsmjyiqwrapeczokghtvdxl
+fondsmjypuwrapeczikghtvjxl
+fonbswjyquwrapeczikgvtydxl
+fonbsmjyqqwrapeczikkhtvdbl
+fonbsmjyquwrapemzitghtvdsl
+fonbsmjyquwrspecziegxtvdxl
+fonbsmpyquwrgpeczikghtwdxl
+fodbsmjqquwrapeczmkghtvdxl
+fonbsmjkquwrapeczikghpvdxr
+fonbsmjyquwrapeczikshzvmxl
+fznbsmjyqulrapeczikghkvdxl
+fonbsmjyquwripeczikghtbdjl
+fcnbsmjyquzrapecyikghtvdxl
+ronbxmjyquwrapeczikghgvdxl
+fonbsmuyvuwrgpeczikghtvdxl
+fonbsmjyyuwraplczikghtudxl
+poxbsmjyqewrapeczikghtvdxl
+foabsmjyquwrapecziqghtvpxl
+ponbsmjrquwrapeczikchtvdxl
+fonzzmjyquwrapeczikghtvdxs
+wonbsmjyquwghpeczikghtvdxl
+fofbsejyquwrapeczikgctvdxl
+ponbsmjyquwrayegzikghtvdxl
+fonbumjyquwripeczikghtvdxf
+fonbsmqyquwrapeczikgftvdxv
+qonbsmjyquwraplczitghtvdxl
+fmnbsajdquwrapeczikghtvdxl
+fonbsrjyquwrapempikghtvdxl
+fonbsmjyquwrapeczikgotudxw
+fonbsmtyquwrapeflikghtvdxl
+fzqbsmjyquwrapecjikghtvdxl
+fdnbsmjyquwraqeclikghtvdxl
+fvnbsijyquwrapechikghtvdxl
+fovbsmjyquwsapeczikghqvdxl
+ffjbsmjyqgwrapeczikghtvdxl
+fonbsmjyquwrapeczvkhhivdxl
+forbamjjquwrapeczikghtvdxl
+fonbwmjyquwtapeyzikghtvdxl
+fonvsmjyquwrapeczikglnvdxl
+fonnsmjyguwrapeczikghtvxxl
+fopbsmjyquwrapeczikghtvaxz
+fonbsmjyquwiapeczikrhavdxl
+fonbsujyquwrapeczikthtvdjl
+fonpsmkyeuwrapeczikghtvdxl
+fonbsmjyquwrapeczqkgttvdxk
+fonbsmjyqzwrapeczikgrtddxl
+fokbsmjiquwrapeczikgltvdxl
+fonbsmjyqbwrapeczikghttdxo
+fonbsejyquwrapeczikghbvdal
+fonblmjyquwyaveczikghtvdxl
+fonbsmjyquwlzpepzikghtvdxl
+fonbsmjyqulrapbczigghtvdxl
+fonbsmjyxuwrapecziyghtvsxl
+fonbyjjyquwrapeczikghtvdxn
+fonbhmjyquwrapeczikghtjhxl
+fonbspjykuwraieczikghtvdxl
+aonbsmjyquwwapeczikchtvdxl
+fombsmjyquwyapeczikghtvdll
+fonbsmjynuwrapeczivgbtvdxl
+xonbsmjfquwrapeczikghqvdxl
+fonbyzjyquwzapeczikghtvdxl
+fbnbsmjyquwrapeczimgvtvdxl
+qonbsmjyquwraoeczikgftvdxl
+fonbsrjyquwrapeczikghtvjxm
+fonbsmjyquwrapxjzykghtvdxl
+fonbwgjyquwrapecziklhtvdxl
+fonjcmjyouwrapeczikghtvdxl
+fonbsmjyquwrapefzisuhtvdxl
+fonbsmjyqywrspeczikghtvnxl
+qonbsmjyquwrapeczlkuhtvdxl
+fonbsmjyqlprapeczikghtvdbl
+fonbsmjzquwrapedzikfhtvdxl
+fonbsmjyquwrapeczizghtvjxq
+fonbsmxyquwrrpeczikghtvcxl
+fonpsmjyquwoapeczikghjvdxl
+fonbshkyauwrapeczikghtvdxl
+fonbsmjysuwrapeczilghpvdxl
+fovwsxjyquwrapeczikghtvdxl
+fonbsmjyquwrppecnikghmvdxl
+fonbkmjyiuwrrpeczikghtvdxl
+gonbsmjyquwrapeczikphtudxl
+foncsmjyqlwrapeczimghtvdxl
+fonbsmjhquwrtpeczikghtvdxg
+fogbsmjyquarapeczikghtvdil
+fonbsmjyquwraperzekghwvdxl
+fonbstjyquwrapeczicghtedxl
+fonbsmjoquhrapeczikgotvdxl
+fonbsmjykuwrareczikgdtvdxl
+fonbsmjyvuwrayeczivghtvdxl
+fonbzmgyquwraptczikghtvdxl
+fonbsmjyqubrapeczikgftvdxb
+fonbgmjyjuwrapeczikghtvdul
+fonbsmjzqurrapeczikghtvfxl
+fonbsmjyiuwrapeczikgstvtxl
+fpnbstjyquwrapeczikghtvdcl
+fonbpmjyquwrapeczivghtndxl
+fonbsmjyquwrapeczilgptvvxl
+fonbsmjyqdwripecbikghtvdxl
+fonbsmjytuwgapnczikghtvdxl
+fonbsejyquwrapedzikghtvdml
+fonbsojyqdwrapeczikghtgdxl
+fonbsmjykuwrayeczicghtvdxl
+foubsmtyquwrapeczikchtvdxl
+fonbqmjyqukrapeyzikghtvdxl
+fonbsmjyquwaapenzikghtvdwl
+fonbsmeyquwrapeyzixghtvdxl
+fonusmjyquhrapeczikgytvdxl
+fonbsmjyquwrapwazikqhtvdxl
+fonwsmeyquwrapeczikghhvdxl
+fonmsmjyquxrspeczikghtvdxl
+fonqsmjyqxwrapeczikghtvdml
+fonfsmjyquwrapeuzikgatvdxl
+fonvsmjyquwrapeczikgrtvdul
+fonbsmayquwrapeczikihtvdxm
+fonbsmnyquwrapecdifghtvdxl
+fonbsmjyeuwraseczikghtvdxo
+fonbvvjyquwrapeczikghtvdxi
+fonbsmjyquwrapeczbkghtorxl
+tonbsmjyqvwrapeczikghtvdcl
+fonbsmjyquwrapeczhkgbtvdkl
+fonqsmjyquwrapenzibghtvdxl
+fontsmeyqudrapeczikghtvdxl
+qonbsmjyauwrapeczikghtvdbl
+fynbsmjyluwrapeczekghtvdxl
+fonbsmjhquwrappczikghtvdxt
+conbsmjyquwrapeczikahtvdxz
+fonbsmjyquorapeczikvftvdxl
+fonbsriyquwrapeczikchtvdxl
+yonfsmjyquwrapeczikghtvdxq
+fonaomjyquwrapecziwghtvdxl
+fonbsxsyqdwrapeczikghtvdxl
+fonbsqjyouwrapeczikgltvdxl
+fonbstsyquwraleczikghtvdxl
+fonbsmjyquwraoecztkghtvdsl
+fonbsmjyquwrapezzjkghmvdxl
+fonbwmjyqnwrapecpikghtvdxl
+fonbsmvyqbwrapeczikghtvdsl
+fonbsijyquwrazeczikghtvdwl
+fonbsmjyouwrapewzikghtldxl
+xonbsmjyqcwrapeczikghtvdul
+fonbgmjxquwrajeczikghtvdxl
+fokbsmjyquwrapechikghtrdxl
+fonbqmjyqawrapeczikghtrdxl
+fonbwmjzquwtapeyzikghtvdxl
+fonbsmjyquwrapecdikgatvdnl
+fonbsmjyqowrkpeczikghtvdxj
+fonbsmjyquwkapejzikuhtvdxl
+fonbsmjyquwrabeozikghtmdxl
+fonbsijyeuwrapeczikghtvdxh
+fonbsmjhquprapeczizghtvdxl
+fonesmjyquwrapcczikghtvdxh
+fonbamjyquwrapeczifrhtvdxl
+foabsmjyquwpapeczikghtvdxs
+fonbsmjyquwrapeczukghivdxh
+fonbsejyoulrapeczikghtvdxl
+fonbsmjyquwraceczikgdmvdxl
+eonbsmjyquerppeczikghtvdxl
+ffnzsmjyquwgapeczikghtvdxl
+donbsmyyquwrapeczirghtvdxl
+fjnbsmjyqufrapeczikghtwdxl
+fonfsmjyquwrareczigghtvdxl
+fonusmjyquwrapeczikgetvexl
+tonbsmjyqpwrapeczikghtjdxl
+fonbsmjhqukkapeczikghtvdxl
+fonbsmjyqusraseczikghtvzxl
+fonbsmjyquygapeczxkghtvdxl
+folbsmjyquwraqeczikghjvdxl
+fonbsmjyquwrppecjinghtvdxl
+fonbsmjyquwraepczhkghtvdxl
+fonbfmjyquwrapeczisghtrdxl
+fsnbsmjwqubrapeczikghtvdxl
+fonbspjyquwrapjczikghtedxl
+fowbsmjyquwrapeczikghtbdbl
+fonbymjyquwrapeczikghlvdrl
+fonbsmjyruwrapecbikghtvixl
+fonyqmjyqufrapeczikghtvdxl
+focbscjyquwrapeczmkghtvdxl
+fonbsmjyqtwnkpeczikghtvdxl
+eonbsmjyquwrameczizghtvdxl
+zonbsmjyqcwrapeczikghtvhxl
+foubsmjyquwrapehzikghtvnxl
+ffnbsmjyquwrapetzikghtjdxl
+fonbjgjyquwrapkczikghtvdxl
+fonbwmjyquwqapeczdkghtvdxl
+forbsmjyquwrapeczikkhtvdml
+fonbsmjyiuwrapeczivghevdxl
+fonbsmjyquwrapeglikghwvdxl
+fopgsmjyquwrapegzikghtvdxl
+fonbsmjyqzwrajeczikghtldxl
+fonbsmjyruwrapexzmkghtvdxl
+fonbsmjyquwrdpeczikxstvdxl
+fonbsmjyquwrapeezivghtvdql
+fonbdmjyqujsapeczikghtvdxl`
+
+const demo1 = `abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab`
+
+const demo2 = `abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz`
+
+const getChecksum = (string) => {
+  let array = string.split('\n');
+  const length = array.length;
+  let current = {}
+  let box = '';
+  let threes = 0;
+  let twos = 0;
+  for (i = 0; i < array.length; i++) {
+    let currentThrees = 0;
+    let currentTwos = 0;
+    box = array[i];
+    // console.log(box)
+    for (a = 0; a < box.length; a++) {
+      if (!current[`${box[a]}`]) {
+        current[`${box[a]}`] = 1;
+      } else {
+        current[`${box[a]}`] = current[`${box[a]}`] + 1;
+      }
+      switch(current[`${box[a]}`]) {
+          case 2:
+          currentTwos++;
+          break;
+
+          case 3:
+          currentTwos--;
+          currentThrees++;
+          break;
+          
+          case 4:
+          currentThrees--;
+          break;
+
+          default:
+          break;
+        }
+    }
+    if (currentThrees > 0) {
+      threes++;
+    }
+    if (currentTwos > 0) {
+      twos++;
+    }
+    // console.log(current)
+    // console.log(threes, twos)
+    current = {};
+  }
+  // console.log(threes, twos)
+  return threes * twos;
+}
+
+const findCommon = (string) => {
+  let array = string.split('\n');
+  const length = array.length;
+  const boxLength = array[0].length;
+  let wrong = 0;
+  let wrongLetter = 0
+  let box1 = "";
+  let box2 = "";
+  for (i = 0; i < length; i++) {
+    box1 = array[i];
+    for (j = i; j < length; j++) {
+      box2 = array[j];
+      for (n = 0; n < boxLength; n++) {
+        if (box1[n] !== box2[n]) {
+          wrongLetter = n;
+          wrong++;
+        }
+        if (wrong > 1) {
+          break;
+        }
+      }
+      if (wrong === 1) {
+        return [box1, box2, box1.substring(0, wrongLetter) + box1.substring(wrongLetter + 1)]
+      } else {
+        wrong = 0;
+      }
+    }
+  }
+}
+
+module.exports = {demo1, demo2, test, getChecksum, findCommon};
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // ----- Day 1
 const day1 = require('./Day-1/day1.js');
-//console.log(day1.findFirstDouble(day1.test))
+// console.log(day1.findFirstDouble(day1.test))
 
 // Alternate (Faster. Suggested by AJ Genung.)
 const day1alt = require('./Day-1/day1-alternate.js');
-//console.log(day1alt.findFirstDouble(day1.test));
+// console.log(day1alt.findFirstDouble(day1.test));
+
+// ----- Day 2
+const day2 = require('./Day-2/day2.js');
+// Part 1
+// console.log(day2.getChecksum(day2.test));
+// Part 2
+// console.log(day2.findCommon(day2.test));
