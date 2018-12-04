7d5b6bdadec1318af308f1d8489da5f575346704
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-12-04 00:54:45 -0700

    Upload Day 3 and Day 4 Solutions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const day1 = require('./Day-1/day1.js');
 // console.log(day1.findFirstDouble(day1.test))
 
-// Alternate (Faster. Suggested by AJ Genung.)
+// Alternate (Faster. Idea Suggested by AJ Genung.)
 const day1alt = require('./Day-1/day1-alternate.js');
 // console.log(day1alt.findFirstDouble(day1.test));
 
@@ -12,3 +12,17 @@ const day2 = require('./Day-2/day2.js');
 // console.log(day2.getChecksum(day2.test));
 // Part 2
 // console.log(day2.findCommon(day2.test));
+
+// ----- Day 3
+const day3 = require('./Day-3/day3.js');
+// Part 1
+// console.log(day3.overlap(day3.demo1, 8));
+// Part 2
+// console.log(day3.noOverlap(day3.test, 1000));
+
+// ----- Day 4
+const day4 = require('./Day-4/day4.js');
+// Part 1
+// console.log(day4.sleepiestGuardStrategy1(day4.test))
+// Part 2
+console.log(day4.sleepiestGuardStrategy2(day4.test))
